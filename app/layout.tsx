d2bb1089ba14db878699f11812f27a35becc65a2
watermelonbit <enoabasidream@gmail.com>
2024-02-19 19:11:31 +0400

    feat: shadcn/ui BYOC

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
+import "@/styles/globals.css"
+
 import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
 
